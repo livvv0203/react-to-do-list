54746683d81a8116706211a7493e0dbecc430688
Jieqing Liu <51498530+livvv0203@users.noreply.github.com>
2022-06-24 12:53:18 -0700

    React bootstrap, UI for btn
    Features about add and delete

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
-import { render } from "@testing-library/react";
 import React, { Component } from "react";
-import Test from "./components/test";
+import TodoList from "./components/to-do-list.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Test />
+        <TodoList />
       </div>
     );
   }
 }
 
 export default App;
-
-
-
-// Class Component
-class NavBar extends Component {
-  render() {
-    return <div id="navbar">NavBar</div>;
-  }
-}
-
-// Function Component
-function Swiper() {
-  return <div>Swiper</div>;
-}
-
-// Arrow Function
-const TabBar = () => {
-  <div>TabBar</div>;
-};
-
 
 
 
--- src/components/to-do-list.js
+++ src/components/to-do-list.js
@@ -1,39 +1,38 @@
 import React, { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../css/index.css";
 
-class Test extends Component {
+class TodoList extends Component {
   state = {
     list: [
       {
         id: 1,
-        text: "aa1",
-      },
-      {
-        id: 2,
-        text: "bbb",
-      },
-      {
-        id: 3,
-        text: "ccc",
-      },
+        text: "TEST",
+      }
     ],
   };
 
   refInput = React.createRef();
 
   render() {
+
     return (
-      <div>
-        <input type={"text"} ref={this.refInput}></input>
-        <button onClick={this.handleClick}>Add</button>
+      <div className="todolist-wrapper">
+        <input type={"text"} ref={this.refInput} className="input-box"></input>
+        <Button variant="secondary" className="btn-add" onClick={this.handleClick}>Add</Button>
+      
         <ul>
           {this.state.list.map((item, index) => (
             <li key={item.id}>
-              {item.text} ------- {index} ----- {item.id}
-              <button>Delete</button>
+              {item.text}
+              {/* <span dangerouslySetInnerHTML={{ __html: item.text }}></span> */}
+              <Button variant="danger" className="btn-delete" onClick={() => this.handleDelete(index)}>Delete</Button>
             </li>
           ))}
         </ul>
+
+        {this.state.list.length === 0 && <div>NO DATA</div>}
       </div>
     );
   }
@@ -47,7 +46,19 @@ class Test extends Component {
       text: this.refInput.current.value,
     });
     this.setState({ list: newList });
+    this.refInput.current.value = " ";
+    
+  };
+
+  handleDelete = (index) => {
+    console.log("Delete: " + index);
+
+    var newList = this.state.list.slice();
+    newList.splice(index, 1); // Delet one from index
+    this.setState({
+      list: newList,
+    });
   };
 }
 
-export default Test;
+export default TodoList;
